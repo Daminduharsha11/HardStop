1cfb0be94a4028a7c1b504f88a0d3d91c7f8788e
Daminduharsha11 <99626715+Daminduharsha11@users.noreply.github.com>
2026-09-18 17:04:50 +0530

    namespace & applicationId fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,11 +10,11 @@ plugins {
 }
 
 android {
-  namespace = "com.example"
+  namespace = "com.aegis.hardstop"
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
-    applicationId = "com.hardstop.detox"
+    applicationId = "com.aegis.hardstop"
     minSdk = 24
     targetSdk = 36
     versionCode = 1
